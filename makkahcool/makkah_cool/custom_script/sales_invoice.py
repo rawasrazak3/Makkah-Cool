889c2b12b0ce4970075c05c1ab9a16477a54a277
rawasrazak3 <rawasrazak3@gmail.com>
2024-09-03 11:19:50 +0000

    customs1

--- makkahcool/makkah_cool/custom_script/sales_invoice.py
+++ makkahcool/makkah_cool/custom_script/sales_invoice.py
@@ -57,5 +57,7 @@ def stock_qty_1(item_code):
     return results
 
 def autoname(doc, method):
-    if doc.set_warehouse == "Shuwaikh Showroom - MCR&ACSP":
-        doc.name= make_autoname(f"S-{doc.naming_series}")
+    if doc.is_pos == 1:
+        doc.name= make_autoname(f"CA-{doc.naming_series}")
+    else:
+        doc.name= make_autoname(f"CR-{doc.naming_series}")
